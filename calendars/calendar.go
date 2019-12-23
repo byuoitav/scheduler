d17060ed441467151d208675caee7b6c75bf2b7f
Daniel Randall <dgrandall@me.com>
2019-12-23 10:18:03 -0700

    added calendar generic stuff

---
+++ calendars/calendar.go
@@ -0,0 +1,85 @@
+package calendars
+
+import (
+	"context"
+	"net/http"
+	"sync"
+
+	"github.com/labstack/echo"
+)
+
+type Event struct {
+	Title     string `json:"title"`
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
+}
+
+type Calendar interface {
+	GetEvents(context.Context) ([]Event, error)
+	CreateEvent(context.Context, Event) error
+}
+
+type CreateCalendarFunc func(context.Context, string) (Calendar, error)
+
+func CreateCalendarServer(create CreateCalendarFunc) (Server, error) {
+	e := newEchoServer()
+	m := &sync.Map{}
+
+	createCal := func(ctx context.Context, roomID string) (Calendar, error) {
+		if cal, ok := m.Load(roomID); ok {
+			return cal.(Calendar), nil
+		}
+
+		cal, err := create(ctx, roomID)
+		if err != nil {
+			return nil, err
+		}
+
+		m.Store(roomID, cal)
+		return cal, nil
+	}
+
+	e.GET("/events/:roomID", func(c echo.Context) error {
+		roomID := c.Param("roomID")
+		if len(roomID) == 0 {
+			return c.String(http.StatusBadRequest, "must include roomID")
+		}
+
+		cal, err := createCal(c.Request().Context(), roomID)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		events, err := cal.GetEvents(c.Request().Context())
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, events)
+	})
+
+	e.POST("/events/:roomID", func(c echo.Context) error {
+		roomID := c.Param("roomID")
+		if len(roomID) == 0 {
+			return c.String(http.StatusBadRequest, "must include roomID")
+		}
+
+		var event Event
+		if err := c.Bind(&event); err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		cal, err := createCal(c.Request().Context(), roomID)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		if err := cal.CreateEvent(c.Request().Context(), event); err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.String(http.StatusOK, "event successfully created")
+	})
+
+	return wrapEchoServer(e), nil
+}
---
+++ calendars/server.go
@@ -0,0 +1,31 @@
+package calendars
+
+import (
+	"net"
+
+	"github.com/labstack/echo"
+)
+
+type Server interface {
+	Serve(lis net.Listener) error
+}
+
+type wrappedEchoServer struct {
+	*echo.Echo
+}
+
+func newEchoServer() *echo.Echo {
+	e := echo.New()
+
+	return e
+}
+
+func wrapEchoServer(e *echo.Echo) Server {
+	return &wrappedEchoServer{
+		Echo: e,
+	}
+}
+
+func (e *wrappedEchoServer) Serve(lis net.Listener) error {
+	return e.Server.Serve(lis)
+}
