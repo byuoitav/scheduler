7db4dca386996324d3877a044e70c2506e63cba4
Andrew Werner <klinc9@gmail.com>
2020-04-07 09:53:18 -0600

    adding websocket count event

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/byuoitav/central-event-system/hub/base"
+	"github.com/byuoitav/central-event-system/messenger"
+	"github.com/byuoitav/common/v2/events"
+	"github.com/byuoitav/device-monitoring/localsystem"
 	"github.com/byuoitav/scheduler/calendars"
 	"github.com/byuoitav/scheduler/log"
 	"github.com/byuoitav/scheduler/schedule"
 	"github.com/labstack/echo"
 	"go.uber.org/zap"
 )
+
+var start time.Time
 
 // GetConfig returns the config for this device, based on it's SYSTEM_ID
 func GetConfig(c echo.Context) error {
@@ -29,11 +36,14 @@ func GetConfig(c echo.Context) error {
 	if err != nil {
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
+	start = time.Now()
+	go connectionCheck()
 
 	return c.JSON(http.StatusOK, config)
 }
 
 func GetEvents(c echo.Context) error {
+	start = time.Now()
 	roomID := c.Param("roomID")
 
 	log.P.Info("Getting events", zap.String("room", roomID))
@@ -59,4 +69,44 @@ func CreateEvent(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, fmt.Sprintf("Successfully created %q in %q", event.Title, roomID))
+}
+
+func connectionCheck() {
+	id := localsystem.MustSystemID()
+	deviceInfo := events.GenerateBasicDeviceInfo(id)
+	roomInfo := events.GenerateBasicRoomInfo(deviceInfo.RoomID)
+	messenger, err := messenger.BuildMessenger(os.Getenv("HUB_ADDRESS"), base.Messenger, 1000)
+	if err != nil {
+		log.P.Error("unable to build websocket count messenger: %s", zap.String("error", err.Error()))
+	}
+	for {
+		var countEvent events.Event
+		log.P.Debug("Time since start", zap.String("time", time.Since(start).String()))
+		if time.Since(start).Seconds() > 120 {
+			//time to worry
+			countEvent.GeneratingSystem = id
+			countEvent.Timestamp = time.Now()
+			countEvent.EventTags = []string{events.DetailState}
+			countEvent.TargetDevice = deviceInfo
+			countEvent.AffectedRoom = roomInfo
+			countEvent.Key = "websocket-count"
+			countEvent.Value = fmt.Sprintf("%d", 0)
+		} else {
+			countEvent.GeneratingSystem = id
+			countEvent.Timestamp = time.Now()
+			countEvent.EventTags = []string{events.DetailState}
+			countEvent.TargetDevice = deviceInfo
+			countEvent.AffectedRoom = roomInfo
+			countEvent.Key = "websocket-count"
+			countEvent.Value = fmt.Sprintf("%d", 1)
+		}
+
+		if messenger != nil {
+			log.P.Debug("Sending websocket count of", zap.String("value", countEvent.Value))
+			messenger.SendEvent(countEvent)
+		}
+
+		time.Sleep(3 * time.Minute)
+	}
+
 }
